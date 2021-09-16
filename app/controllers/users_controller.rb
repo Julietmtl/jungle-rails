225f73093144cad525cbad7786d8f518039e5f79
Juliet Li <li_juliet@hotmail.com>
2021-09-16 19:50:14 +0000

    add user authentication and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,15 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
-
     else
       redirect_to '/users/new'
   end
+end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-  
+
 end
